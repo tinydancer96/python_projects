7d64052b98b476ac2dc3e1f250fbce8bb9db5653
Mardgelle Neri <mardgelleneri@gmail.com>
2024-01-31 23:11:08 +0000

    added djago form, csrf token, sessions

--- intro_to_django/tasks/views.py
+++ intro_to_django/tasks/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
-tasks = ["foo", "bar", "baz"]
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task:")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
         # "tasks" - this is what the HTML is accessing. the information of this key is determined by it's value pair (the variable)
         # : tasks - this is the variable and contains the information
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
