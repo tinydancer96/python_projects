04bc24d31499f5be84e764d3f69cd87a8b26a4e3
Mardgelle Neri <mardgelleneri@gmail.com>
2024-01-31 20:23:40 +0000

    tasks app made

---
+++ intro_to_django/tasks/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+tasks = ["foo", "bar", "baz"]
+
+# Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+        # "tasks" - this is what the HTML is accessing. the information of this key is determined by it's value pair (the variable)
+        # : tasks - this is the variable and contains the information
+    })
+
+def add(request):
+    return render(request, "tasks/add.html")
