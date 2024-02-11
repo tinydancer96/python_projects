656a85c19be54f87e43d683876c6f98af6f5026e
Mardgelle Neri <mardgelleneri@gmail.com>
2024-02-11 21:57:25 +0000

    edit function started. url added. function and html to be made

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("encyclopedia/<str:entry>", views.entry, name="entry"),
     path("query/", views.query, name="query"),
-    path("new_entry", views.new_entry, name="new_entry")
+    path("new_entry", views.new_entry, name="new_entry"),
+    path("encyclopedia/<str:entry>/edit", views.edit, name="edit")
 ]
