fb4f8160d51a115b5cd91e70ceef6a499b1295e4
Mardgelle Neri <mardgelleneri@gmail.com>
2024-02-21 00:06:49 +0000

    wip

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("entries/<str:entry>", views.entry, name="entry"),
     path("query/", views.query, name="query"),
     path("new_entry", views.new_entry, name="new_entry"),
-    path("edit/<str:entry>", views.edit, name="edit")
+    path("edit/", views.edit, name="edit"),
+    path("save_edit/", views.save_edit, name="save_edit")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,10 +7,6 @@ class NewEntryForm(forms.Form):
     title = forms.CharField(label="Title")
     content = forms.CharField(label="Content")
 
-class EditEntryForm(forms.Form):
-    title = forms.CharField(label="Title", disabled=True)
-    content = forms.CharField(label="Content")
-
 def index(request):
     entries = util.list_entries()
     entry_contents = {}
@@ -58,26 +54,21 @@ def new_entry(request):
         "new_entry": NewEntryForm()
         })
 
-# def edit(request, entry):
-#     entry_content = util.get_entry(entry)
-#     if request.method == "POST":
-#         edit_entry = EditEntryForm(request.POST)
-#         if edit_entry.is_valid():
-#             util.save_entry(entry, edit_entry.cleaned_data['content'])
-#             return redirect("entry", entry=entry.lower())
-#         else:
-#             return render(request, "encyclopedia/edit.html", {
-#                 "edit_entry": EditEntryForm(initial={'title': entry, 'content': "Invalid content"})
-#             })
-#     else:
-#         return render(request, "encyclopedia/edit.html", {
-#         "edit_entry": EditEntryForm(initial={'title': entry, 'content': entry_content}),
-#         "entry_title": entry
-#         })
-
-def edit(request, entry):
-    entry_content = util.get_entry(entry)
-    return render(request, "encyclopedia/edit.html", {
-        "edit_entry": EditEntryForm(initial={'title': entry, 'content': entry_content}),
-        "entry_title": entry
+def edit(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        return render(request, "encyclopedia/edit.html", {
+            "title": title,
+            "content": content
         })
+
+def save_edit(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        if util.get_entry(title) is not None:
+            util.save_entry(title, content)
+            return redirect("entry", entry=title)
+    else:
+        return redirect(index)
