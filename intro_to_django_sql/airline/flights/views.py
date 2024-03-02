e0602487eb43deaae97f472fdbaf4f9e6a6f334d
Mardgelle Neri <mardgelleneri@gmail.com>
2024-03-02 21:34:12 +0000

    urls and models

--- intro_to_django_sql/airline/flights/views.py
+++ intro_to_django_sql/airline/flights/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .models import Flight
 
 # Create your views here.
+
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
