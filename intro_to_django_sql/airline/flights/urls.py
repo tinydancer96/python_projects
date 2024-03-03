b47248eac3b1144c1202e2c48a2b15bf05b7bfe5
Mardgelle Neri <mardgelleneri@gmail.com>
2024-03-03 10:30:17 +0000

    flight and flight error added

--- intro_to_django_sql/airline/flights/urls.py
+++ intro_to_django_sql/airline/flights/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:flight_id>", views.flight, name="flight"),
+    path("error", views.error, name="error")
 ]
--- intro_to_django_sql/airline/flights/views.py
+++ intro_to_django_sql/airline/flights/views.py
@@ -8,3 +8,17 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+    except Flight.DoesNotExist:
+        return render(request, "flights/error.html", {
+            "message": f"Flight {flight_id} does not exist",
+        })
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
+
+def error(request):
+    return render(request, "flights/error.html")
