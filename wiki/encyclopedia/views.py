86080af1a9d3a1920db799572fa28d8f0e6f6634
Mardgelle Neri <mardgelleneri@gmail.com>
2024-03-12 16:57:39 +0000

    wiki added

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect
+
+from . import util
+
+import random
+
+import markdown
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, entry):
+    content = convert_md_to_html(entry)
+    if content == None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "This page entry does not exist"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": entry,
+            "content": content
+        })
+
+def error(request):
+    return render(request, "encyclopedia/error.html")
+
+
+def new_entry(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        if util.get_entry(title) == None:
+            util.save_entry(title, content)
+            return redirect("entry", entry=title)
+        else:
+            return render(request, "encyclopedia/error.html", {
+                "message": f"This page exists. Please update {title}"
+            })
+    return render(request, "encyclopedia/new_entry.html")
+
+def search(request):
+    if request.method == "POST":
+        search = request.POST.get('q')
+        entry = util.get_entry(search)
+        if entry == None:
+            return render(request, "encyclopedia/error.html", {
+                "message": f"'{search}' does not exist"
+            })
+        else:
+            return redirect("entry", entry=search)
+
+
+def edit(request, entry):
+    content = util.get_entry(entry)
+    if request.method == "POST":
+        return render(request, "encyclopedia/edit.html", {
+            "entry": entry,
+            "content": content
+        })
+
+
+def save_edit(request, entry):
+    content = request.POST.get('content')
+    if request.method == "POST":
+        util.save_entry(entry, content)
+        return redirect("entry", entry=entry)
+
+
+def random_entry(request):
+    list_entries = util.list_entries()
+    random_entry = random.choice(list_entries)
+    return redirect("entry", entry=random_entry)
---
+++ wiki/entries/Python.md
@@ -0,0 +1,8 @@
+# Python
+
+Python is a programming language that can be used both for writing **command-line scripts** or building **web applications**.
+
+
+EDITED
+
+EDITED
