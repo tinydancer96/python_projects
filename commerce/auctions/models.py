d376dd5762a6eb81bfb697fea80ac98bac90e434
Mardgelle Neri <mardgelleneri@gmail.com>
2024-03-13 23:26:31 +0000

    images added

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -13,7 +13,7 @@ class Listing(models.Model):
     categories = models.ForeignKey('Category', related_name='listings', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=1000, decimal_places=2)
     user = models.ForeignKey(User, related_name='listings', on_delete=models.CASCADE)
-
+    image = models.TextField(blank=True)
     def __str__(self):
         return f"{self.title}"
 
